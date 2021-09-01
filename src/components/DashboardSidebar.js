6c8564f56ccfcb4be1b49a580fa751662a5ad668
Dani <dani.zion87@gmail.com>
2021-09-01 10:43:12 +0300

    side bar role updated

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
@@ -21,6 +21,7 @@ import {
   Users as UsersIcon
 } from 'react-feather';
 import NavItem from './NavItem';
+import {createApiClient} from "../api";
 
 const user = {
   avatar: '/static/images/avatars/neta.jpg',
@@ -28,124 +29,172 @@ const user = {
   name: 'Neta Sharon'
 };
 
-const items = [
-  {
-    href: '/app/dashboard',
-    icon: BarChartIcon,
-    title: 'Dashboard'
-  },
-  {
-    href: '/app/monthlySummary',
-    icon: UsersIcon,
-    title: 'Monthly summary'
-  },
-  {
-    href: '/app/upload',
-    icon: ShoppingBagIcon,
-    title: 'Upload CSV'
-  },
-];
+// let items = [
+//   {
+//     href: '/app/dashboard',
+//     icon: BarChartIcon,
+//     title: 'Dashboard'
+//   },
+//   {
+//     href: '/app/monthlySummary',
+//     icon: UsersIcon,
+//     title: 'Monthly summary'
+//   },
+//   // {
+//   //   href: '/app/upload',
+//   //   icon: ShoppingBagIcon,
+//   //   title: 'Upload CSV'
+//   // },
+// ];
 
 const DashboardSidebar = ({ onMobileClose, openMobile }) => {
   const location = useLocation();
+  const [items, setItems] = useState([
+    {
+      href: '/app/dashboard',
+      icon: BarChartIcon,
+      title: 'Dashboard'
+    },
+    {
+      href: '/app/monthlySummary',
+      icon: UsersIcon,
+      title: 'Monthly summary'
+    },
+    // {
+    //   href: '/app/upload',
+    //   icon: ShoppingBagIcon,
+    //   title: 'Upload CSV'
+    // },
+  ])
 
-  useEffect(() => {
+  const [isAdmin, setIsAdmin] = useState(false)
+
+
+  const setUserPermissions = async (id)  =>{//An example of how to make an api call for the server
+    let apiClient = createApiClient();
+    let res = await apiClient.getUserData(id)
+    console.log(res)
+    if(res === 'ADMIN'){
+      setIsAdmin(true)
+      setItems([
+        {
+          href: '/app/dashboard',
+          icon: BarChartIcon,
+          title: 'Dashboard'
+        },
+        {
+          href: '/app/monthlySummary',
+          icon: UsersIcon,
+          title: 'Monthly summary'
+        },
+        {
+          href: '/app/upload',
+          icon: ShoppingBagIcon,
+          title: 'Upload CSV'
+        },
+      ])
+    }
+  }
+
+
+  useEffect(async () => {
+    await setUserPermissions('userId69')
     if (openMobile && onMobileClose) {
       onMobileClose();
     }
   }, [location.pathname]);
 
   const content = (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%'
-      }}
-    >
       <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          p: 2
-        }}
-      >
-        <Avatar
-          component={RouterLink}
-          src={user.avatar}
           sx={{
-            cursor: 'pointer',
-            width: 64,
-            height: 64
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%'
           }}
-          to="/app/account"
-        />
-        <Typography
-          color="textPrimary"
-          variant="h5"
+      >
+        <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              p: 2
+            }}
         >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {user.jobTitle}
-        </Typography>
+          <Avatar
+              component={RouterLink}
+              src={user.avatar}
+              sx={{
+                cursor: 'pointer',
+                width: 64,
+                height: 64
+              }}
+              to="/app/account"
+          />
+          <Typography
+              color="textPrimary"
+              variant="h5"
+          >
+            {user.name}
+          </Typography>
+          <Typography
+              color="textSecondary"
+              variant="body2"
+          >
+            {user.jobTitle}
+          </Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ p: 2 }}>
+          <List>
+            {items.map((item) => (
+                <NavItem
+                    href={item.href}
+                    key={item.title}
+                    title={item.title}
+                    icon={item.icon}
+                    onClick
+                />
+            ))}
+          </List>
+        </Box>
+        <Box sx={{ flexGrow: 1 }} />
       </Box>
-      <Divider />
-      <Box sx={{ p: 2 }}>
-        <List>
-          {items.map((item) => (
-            <NavItem
-              href={item.href}
-              key={item.title}
-              title={item.title}
-              icon={item.icon}
-              onClick
-            />
-          ))}
-        </List>
-      </Box>
-      <Box sx={{ flexGrow: 1 }} />
-    </Box>
   );
 
   return (
-    <>
-      <Hidden lgUp>
-        <Drawer
-          anchor="left"
-          onClose={onMobileClose}
-          open={openMobile}
-          variant="temporary"
-          PaperProps={{
-            sx: {
-              width: 256
-            }
-          }}
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-      <Hidden lgDown>
-        <Drawer
-          anchor="left"
-          open
-          variant="persistent"
-          PaperProps={{
-            sx: {
-              width: 256,
-              top: 64,
-              height: 'calc(100% - 64px)'
-            }
-          }}
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-    </>
+      <>
+        <Hidden lgUp>
+          <Drawer
+              anchor="left"
+              onClose={onMobileClose}
+              open={openMobile}
+              variant="temporary"
+              PaperProps={{
+                sx: {
+                  width: 256
+                }
+              }}
+          >
+            {content}
+          </Drawer>
+        </Hidden>
+        <Hidden lgDown>
+          <Drawer
+              anchor="left"
+              open
+              variant="persistent"
+              PaperProps={{
+                sx: {
+                  width: 256,
+                  top: 64,
+                  height: 'calc(100% - 64px)'
+                }
+              }}
+          >
+            {content}
+          </Drawer>
+        </Hidden>
+      </>
   );
 };
 
